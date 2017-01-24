b01ab7047e6a2bb8f52b34aba3fd5b0fa1037a10
emohat1r <andreyws96@gmail.com>
2017-01-24 23:30:02 +0100

    Changed plot output a bit

--- plot_chart.py
+++ plot_chart.py
@@ -15,21 +15,23 @@ def plot_func(*args):
     averages = []
     if len(args) > 0:
         file_name = args[0]
-        file_image = file_name.replace('data/data', 'images/img')
+        file_image = file_name.replace('data/data', 'images/image')
         file_image = file_image.replace('csv', 'png')
     else:
         file_name = FILE_DATA
         file_image = FILE_IMAGE
-    print(file_name, end=' ')
-    print(file_image, end = ' ')
     with open(file_name, 'r') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',')
         for row in spamreader:
-            print(str(row))
             iterations.append(float(row[0]))
             scores.append(float(row[1]))
             # calculating trend
             averages.append(np.mean(scores[-TREND_ACCURACY:]))
+    plt.xlim(len(iterations) + 150)
+    plt.gca().invert_xaxis()
+    plt.xlabel('Iterations')
+    plt.ylabel('Scores')
+    plt.title("Results for: " + file_image.replace('images/image_',''))
     # plotting real values
     plt.plot(iterations, scores, 'r.')
     # plotting trend
