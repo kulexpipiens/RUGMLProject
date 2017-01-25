614928c041f0ca9118e028e975e9f8c16e8f1b91
Martin Glova <martin.glova@student.upjs.sk>
2017-01-25 12:42:16 +0100

    newest results

--- plot2.py
+++ plot2.py
@@ -10,7 +10,7 @@ DPI=500
 TREND_ACCURACY=1000
 
 colors = ["#000000", "#0000FF", "#A52A2A", "#7FFF00", "#DC143C", "#006400", "#FF8C00", "#FF1493", "#FFD700", "#808080", "#669900", "#ff66ff"]
-indices = ["1.0", "0.99999","0.9","0.8","0.7","0.6","0.5","0.4","0.3","0.2","0.1","1e-05"]
+indices = ["1.0","0.9","0.8","0.7","0.6","0.5","0.4","0.3","0.2","0.1"]
 col = {}
 notplotlr = []#['0.4', '0.7']
 
@@ -37,11 +37,11 @@ def add_to_plot(file_name, i, lr):
 i=0
 for file in glob.glob('data/data_*'):
 	namea = file.split('_')
-	change = namea[2] == '15' and namea[6] == '1.0' and namea[8].split('.')[0] == '5' and len(namea) < 10
+	change = namea[2] == '15' and namea[4] == '0.7' and namea[8].split('.')[0] == '5' and len(namea) < 10
 	if change:
-		add_to_plot(file, i, namea[4])
+		add_to_plot(file, i, namea[6])
 		i+=1
 for idx in indices:
 	plt.plot(col[idx][0], col[idx][1], col[idx][2], label=idx, linewidth=2)
 plt.legend(bbox_to_anchor=(1, 1), loc=1, borderaxespad=.5)
-plt.savefig("images2/plot1_TA"+str(TREND_ACCURACY)+"without"+str(notplotlr)+"_new_new.png", dpi=DPI)
+plt.savefig("images2/plot2_TA"+str(TREND_ACCURACY)+"without"+str(notplotlr)+"_new.png", dpi=DPI)
